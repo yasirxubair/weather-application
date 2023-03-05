ac12d288b1fd1894792caf2451c6c3252d975c51
yasirxubair <yasir.xubair@gmail.com>
2023-03-05 21:11:27 +0500

    Add .gitattributes file

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4f402060c9msh5902f92e03b65abp1ef453jsn061bfe8ce27a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "8a912d95a5b1e9cb144040a2da821dc8";
